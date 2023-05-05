84c5810e90c09856c2ecd01d8871b3faf18d17d6
ycryptx <yonatan.med@gmail.com>
2023-05-05 16:31:56 +0900

    add logger

--- cmd/chaincannon/main.go
+++ cmd/chaincannon/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"os"
 	"runtime"
 	"strings"
 
 	"github.com/ycryptx/chaincannon/pkg/benchmark"
 	"github.com/ycryptx/chaincannon/pkg/cosmos"
 	"github.com/ycryptx/chaincannon/pkg/flags"
+	"github.com/ycryptx/chaincannon/pkg/logger"
 )
 
 var SUPPORTED_CHAINS = []string{"cosmos"}
@@ -17,6 +18,7 @@ var SUPPORTED_CHAINS = []string{"cosmos"}
 var txPaths flags.StringArray
 
 func main() {
+	log := logger.InitLogger()
 	chain := flag.String("chain", "", fmt.Sprintf("The blockchain type (e.g. %s).", strings.Join(SUPPORTED_CHAINS, ", ")))
 	endpoint := flag.String("endpoint", "", "The node's RPC endpoint to call.")
 	flag.Var(&txPaths, "tx-file", "Path to a file containing signed transactions. This flag can be used more than once.")
@@ -26,14 +28,12 @@ func main() {
 	flag.Parse()
 
 	recipe := benchmark.ProcessFlags(*endpoint, txPaths, *duration, *amount, *threads, runtime.NumCPU())
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "log", log)
 
 	switch *chain {
 	case "cosmos":
-		err := cosmos.Handle(recipe)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
+		cosmos.Handle(ctx, recipe)
 	default:
 		fmt.Printf("Chaincannon is a blockchain benchmarking tool. Currently supported chains are: %s\n", strings.Join(SUPPORTED_CHAINS, ", "))
 		fmt.Println("Usage: chaincannon [opts]")
